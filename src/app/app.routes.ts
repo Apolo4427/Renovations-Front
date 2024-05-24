7e0141764a6445af325b0261c7fc164b379426b3
apolo4427 <top152695@gmail.com>
2024-05-23 21:36:47 -0500

    iniciando el frontEnd de la aplicacion Renovations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ClienteComponent } from './cliente/cliente.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'', component: ClienteComponent
+    },
+    {
+        path:'**', redirectTo:'', pathMatch:'full'
+    }
+];
